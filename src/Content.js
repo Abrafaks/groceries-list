151559f3fdf67b3535e1515240731ade45eb55db
Your Name <you@example.com>
2024-03-01 16:36:45 +0100

    Move item-related logic to its component

--- src/Content.js
+++ src/Content.js
@@ -1,35 +1,14 @@
-import { FaTrashAlt } from "react-icons/fa";
+import ItemList from "./ItemList";
 
 const Content = ({ items, deleteItem, toggleCheckbox }) => {
     return (
         <main>
             {items.length ? (
-                <ul>
-                    {items.map((item) => (
-                        <li className="item" key={item.id}>
-                            <input
-                                onChange={() => toggleCheckbox(item.id)}
-                                type="checkbox"
-                                checked={item.checked}
-                            />
-                            <label
-                                onDoubleClick={() => toggleCheckbox(item.id)}
-                                style={
-                                    item.checked
-                                        ? { textDecoration: "line-through" }
-                                        : null
-                                }
-                            >
-                                {item.item}
-                            </label>
-                            <FaTrashAlt
-                                role="button"
-                                tabIndex="0"
-                                onClick={() => deleteItem(item.id)}
-                            />
-                        </li>
-                    ))}
-                </ul>
+                <ItemList
+                    items={items}
+                    deleteItem={deleteItem}
+                    toggleCheckbox={toggleCheckbox}
+                />
             ) : (
                 <p style={{ marginTop: "2rem" }}>Your list is empty</p>
             )}
